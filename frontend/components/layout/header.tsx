c047ed768bf03c40c7922699ec8711c1b7047bd0
anshugarg401 <92512766+anshugarg401@users.noreply.github.com>
2024-04-12 00:35:27 +0530

    web3 wallet connect

--- frontend/components/layout/header.tsx
+++ frontend/components/layout/header.tsx
@@ -5,16 +5,55 @@ import NavDrawer from './nav-drawer'
 import {AppContext} from "@/context/app-context"
 import { formatAddress } from '@/lib/utils'
 // import {Pi } from "react-icons/pi"
-
+import {   useEthers ,useEtherBalance} from '../../node_modules/@usedapp/core'
+import { Localhost } from '@usedapp/core'
 const Header = () => {
+  const { account,deactivate, activateBrowserWallet , chainId } = useEthers()
+ 
+
+  const etherBalance = useEtherBalance(account, { chainId: 11155111 })
   const appCtx = useContext(AppContext)
 
-  const {connected, signerAddress, setup} = appCtx
+  const {connected, signerAddress} = appCtx
+ 
+  const ConnectButton = () => (
+    <div>
+      <button onClick={() => activateBrowserWallet()}>Connect</button>
+    </div>
+  )
+  const MetamaskConnect = () => (
+    <div>
+         <div className = "flex flex-row">
+      
+     
+      <div className={`rounded-full w-4 h-4 mr-2 mt-1.5 ${account ? "bg-blue-400" : "bg-red-400"}`}>
 
-  const handleConnect = () => {
-    setup()
-  }
+      </div>
+      {!account && <ConnectButton /> }
+      {account && <button onClick={deactivate}>Disconnect</button>}
+      
+      <br />
+      
+    </div>
+    <div>
+       {account && (
+      <div  className="absolute">
+        <div>
+         
+          &nbsp;
+          <div className="account">{account}</div>
+        </div>
+        <br />
+      </div>
+    )}
+    </div>
+    </div>
+   
+   
+    
+  )
 
+  
   /*
     
 
@@ -32,9 +71,10 @@ const Header = () => {
       <Nav />
 
       <div className='flex items-center'>
-        {!connected && <button className="btn btn-contained btn-small mr-4 hidden md:inline-block" onClick={handleConnect}>Connect</button>}
+      <MetamaskConnect/>
+    
       <div className='mr-4 flex items-center'>
-          <div className={`rounded-full w-4 h-4 mr-4 ${connected ? "bg-blue-400" : "bg-red-400"}`}></div>
+        
           {connected && <div>{formatAddress(signerAddress!)}</div>}
       </div>
       <NavDrawer />
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import { Mainnet, DAppProvider, Config, Sepolia } from '@usedapp/core';
+import { getDefaultProvider } from 'ethers'
+import { Localhost } from '@usedapp/core'
+const config: Config = {
+  // readOnlyChainId: Mainnet.chainId,
+  readOnlyChainId: Localhost.chainId,
+  readOnlyUrls: {
+    // [Mainnet.chainId]: getDefaultProvider('mainnet'),
+    [11155111]: getDefaultProvider("https://eth-sepolia.g.alchemy.com/v2/-rXBeb7Wwqlhn4ewabMzjkrMMn4tQBtU"),
+    // [Localhost.chainId]: 'http://127.0.0.1:8545',
+  },
+}
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <DAppProvider config={config}>
+    <Component {...pageProps} />
+    </DAppProvider>
+  )
 }
